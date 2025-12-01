3d46940a61cf6e0c9107a97b9ca63e7ed3de5a18
Tamás Szelei <szelei.t@gmail.com>
2025-12-01 23:05:26 +0100

    1

---
+++ 1/py/solve.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+
+def parse(file: Path):
+    with file.open() as fp:
+        while line := fp.readline():
+            yield int(line.strip().replace("R", "").replace("L", "-"))
+
+
+def solve1(data: list[int]):
+    val = 50
+    cnt = 0
+    for n in data:
+        val = (val + n) % 100
+        if val == 0:
+            cnt += 1
+    return cnt
+
+
+def solve2(data: list[int]):
+    val = 50
+    cnt = 0
+    for n in data:
+        if n > 0:
+            cnt += (val + n) // 100 - val // 100
+        elif n < 0:
+            cnt += (val - 1) // 100 - (val + n - 1) // 100
+        val = (val + n) % 100
+    return cnt
+    
+
+nums = list(parse(Path("../input.txt")))
+print(solve1(nums))
+print(solve2(nums))
+
+
